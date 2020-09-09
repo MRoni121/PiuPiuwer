84578d8a739c9c28eef044c495fa9c346579e3f8
MRoni121 <mateus-roni@hotmail.com>
2020-09-09 11:59:56 -0300

    Falta fixar

--- Feed.js
+++ Feed.js
@@ -1,18 +1,73 @@
 getData();
-var contador = 0;
 
 var piar = document.querySelector('.piar');
 piar.addEventListener('click', publicar);
 
 var texto = document.querySelector('textarea');
-var caracteres = document.createElement('p');
+var caracteres = document.querySelector('.caracteres');
 caixaDoPiu = document.querySelector('.piar');
 caracteres.textContent = texto.value.length + '/140';
-caixaDoPiu.appendChild(caracteres);
 
-texto.addEventListener('input', function() {
+textoAviso = document.querySelector('.textoPiu');
+texto.addEventListener('input', mudaCorTexto);
+
+
+
+/*muda a cor do texto*/
+function mudaCorTexto(){
     caracteres.textContent = texto.value.length + '/140';
-});
+    if (texto.value.length <= 140){
+        if(textoAviso.classList.contains('transparente') == false){
+            textoAviso.classList.add('transparente');
+        }
+    }
+
+    if(texto.value.length <= 40){
+        if(caracteres.classList.contains('amarelo') == true){
+            caracteres.classList.remove('amarelo');
+        }
+        else if(caracteres.classList.contains('laranja') == true){
+            caracteres.classList.remove('laranja');
+        }
+        else if(caracteres.classList.contains('vermelho')){
+            caracteres.classList.remove('vermelho');
+        }
+    }
+
+    
+    else if(texto.value.length <= 80){
+        caracteres.classList.add('amarelo');
+        if(caracteres.classList.contains('laranja') == true){
+            caracteres.classList.remove('laranja');
+        }
+        else if(caracteres.classList.contains('vermelho')){
+            caracteres.classList.remove('vermelho');
+        }
+    }
+
+
+    else if(texto.value.length <= 140){
+        caracteres.classList.add('laranja');
+        if(caracteres.classList.contains('amarelo') == true){
+            caracteres.classList.remove('amarelo');
+        }
+        else if(caracteres.classList.contains('vermelho')){
+            caracteres.classList.remove('vermelho');
+        }
+    }
+
+
+    else{
+        caracteres.classList.remove('laranja');
+        caracteres.classList.add('vermelho');
+        if(textoAviso.classList.contains('transparente')){
+            textoAviso.classList.remove('transparente');
+        }
+
+
+    }
+     
+}
 
 /*publica um novo piu*/
 function publicar(){
@@ -20,12 +75,12 @@ function publicar(){
     var usuario = "@mateusroni";
     var fotoDePerfil = "./Imagens/Foto.jpeg";
 
-    if(texto.value.length > 0){
-        adicionaPiu(nome, usuario, fotoDePerfil, texto.value);
+    if(texto.value.length > 0 && texto.value.length <=140){
+        adicionaPiu(nome, usuario, fotoDePerfil, texto.value, 'antes');
         texto.value = '';
         caracteres.textContent = texto.value.length + '/140';
-
     }
+    mudaCorTexto();
 }
 
 /*faz o request http e chama a adicionaPiu*/
@@ -35,14 +90,14 @@ function getData() {
     xhr.onload = () =>{
         const data = JSON.parse(xhr.response);
         //console.log(data);
-        for(var i = data.length - 1; i >= 0; i--)
-            adicionaPiu(data[i].nome, data[i].username, data[i].imagem, data[i].mensagem);
+        for(var i = 0; i < data.length; i++)
+            adicionaPiu(data[i].nome, data[i].username, data[i].imagem, data[i].mensagem, 'depois');
     }
     xhr.send();
 }
 
 /*adiciona piu*/
-function adicionaPiu(nome, usuario, imagem, mensagem){
+function adicionaPiu(nome, usuario, imagem, mensagem, posicao){
     var novosPius = document.querySelector('.novosPius');
 
     var logo = document.createElement('img');
@@ -87,6 +142,7 @@ function adicionaPiu(nome, usuario, imagem, mensagem){
     /*parte de baixo do piu*/
     var likeImagem = document.createElement('img');
     likeImagem.src = "./Imagens/gostar.svg";
+    likeImagem.classList.add('semCor');
 
     var likeBotao = document.createElement('button');
     likeBotao.classList.add('like')
@@ -119,13 +175,50 @@ function adicionaPiu(nome, usuario, imagem, mensagem){
     var piuCompleto = document.createElement('div');
     piuCompleto.appendChild(piu1);
     piuCompleto.appendChild(piu2);
-    piuCompleto.id = contador;
-    novosPius.appendChild(piuCompleto);
-    //console.log(contador);
-    contador++;
-}
 
-/*function apagaPiu(){
-    
+    var fixados = document.querySelector('.fixados');
+
+    if(posicao == 'depois'){
+        novosPius.appendChild(piuCompleto);
+    }
+    else if (posicao == 'antes'){
+        novosPius.prepend(piuCompleto);
+    }
+
+    /*dá like*/
+    likeBotao.addEventListener('click', function(){
+        if(likeImagem.classList.contains('semCor')){
+            likeImagem.classList.add('fundoVermelho');
+            likeImagem.classList.remove('semCor');
+        }
+
+        else{
+        likeImagem.classList.add('semCor');
+        likeImagem.classList.remove('fundoVermelho');
+        }
+    })
+
+    /*fixa um piu, mas o piu desfixado não funciona mais*/
+    var novoPiuCompleto = piuCompleto.cloneNode(true);
+    pinBotao.addEventListener('click', function() {
+        if(piuCompleto.parentNode.className == 'novosPius'){
+            novosPius.insertBefore(novoPiuCompleto, piuCompleto);
+            document.querySelector('.fixados').prepend(piuCompleto);
+            novoPiuCompleto.classList.add('escondido');
+            pinImagem.classList.add('fundoVermelho');
+        }
+        else{
+            a = this.parentNode.parentNode.parentNode;
+            fixados.removeChild(a);
+            novoPiuCompleto.classList.remove('escondido');
+        }
+
+    })
+
+    /*remove*/
+    closeBotao.addEventListener('click', function(){
+        this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode);
+    })
+
+
 }
-*/
